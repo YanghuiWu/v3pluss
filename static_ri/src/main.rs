2677b9fdaf6444cba3ff1ef0dcfa45084f6d3731
Yanghui (Woody) Wu <WoodyY.Wu@outlook.com>
2024-06-07 11:03:46 -0400

    Moved static_ri dependent code from dace to static_ri. Refined static_ri/sampling.rs

--- static_ri/src/main.rs
+++ static_ri/src/main.rs
@@ -1,9 +1,20 @@
+#![allow(internal_features)]
+#![feature(core_intrinsics)]
+
+use std::collections::BTreeSet;
+use std::intrinsics::ceilf32;
+use std::ops::Range;
+use std::{collections::HashMap, time::Instant};
+
+use rand::prelude::Distribution;
+use tracing::debug;
+use tracing_subscriber::EnvFilter;
+
+use dace::ast::{LoopBound, LoopStmt, Node, Stmt};
+use dace_tests::polybench;
+
 mod ri_utils;
 mod sampling;
-use dace::arybase;
-use dace_tests::polybench;
-use std::{collections::HashMap, time::Instant};
-use tracing_subscriber::EnvFilter;
 
 fn main() {
     tracing_subscriber::fmt()
@@ -17,11 +28,98 @@ fn main() {
     // let hist = static_rd::trace::tracing_ri(&mut trace);
     let _hist = sampling::tracing_ri(&mut trace);
     let mut ans = HashMap::new();
-    arybase::sample_collect(&trace, &mut wrapping_loop, &mut ans, &mut ref_counter);
+    sample_collect(&trace, &mut wrapping_loop, &mut ans, &mut ref_counter);
     let _samples: HashMap<usize, std::collections::BTreeSet<Vec<usize>>> =
-        arybase::sample_gen(&mut ans, 0.1);
+        sample_gen(&mut ans, 0.1);
     let end = Instant::now();
     println!("gemm trace time: {:?}", end - start);
     // println!("collected: {ans:#?}");
     //println!("hist: {}", hist);
+}
+
+pub fn sample_collect<'a>(
+    code_tree: &'a Node,
+    wrapping_loops: &mut Vec<&'a LoopStmt>,
+    ans: &mut HashMap<usize, Vec<(&'a str, Range<usize>)>>,
+    // access_name, (loop_name + sample_times)* + -
+    ref_counter: &mut usize,
+) {
+    // let init = (HashMap::<String, usize>::new(), 0);
+    match &code_tree.stmt {
+        Stmt::Loop(stmt) => {
+            wrapping_loops.push(stmt);
+            for i in stmt.body.iter() {
+                sample_collect(i, wrapping_loops, ans, ref_counter);
+            }
+            wrapping_loops.pop();
+        }
+        Stmt::Ref(_) => {
+            let accesses: Vec<_> = wrapping_loops
+                .iter()
+                .filter_map(|x| {
+                    let LoopBound::Fixed(lb) = x.lb else {
+                        return None;
+                    };
+                    let LoopBound::Fixed(ub) = x.ub else {
+                        return None;
+                    };
+                    Some((x.iv.as_str(), lb as usize..ub as usize))
+                })
+                .collect();
+            // let ary_name = x.name;
+            // we could use this to provide more information...
+            ans.insert(*ref_counter, accesses);
+            *ref_counter += 1;
+        }
+        Stmt::Block(x) => {
+            for i in x.iter() {
+                sample_collect(i, wrapping_loops, ans, ref_counter);
+            }
+        }
+        Stmt::Branch(_) => unimplemented!("branch is not supported yet"),
+    }
+}
+
+pub fn sample_gen(
+    collected: &mut HashMap<usize, Vec<(&str, Range<usize>)>>,
+    sampling_rate: f32,
+) -> HashMap<usize, BTreeSet<Vec<usize>>> {
+    let mut intermidiate = HashMap::<usize, f32>::new();
+    collected.iter_mut().for_each(|(ref_id, accesses)| {
+        let mut sample_times = 1.;
+        accesses.iter_mut().for_each(|(_, range)| {
+            let (lb, ub) = (range.start, range.end);
+            sample_times *= (ub - lb) as f32 * sampling_rate;
+        });
+        intermidiate.insert(*ref_id, sample_times);
+    });
+    debug!("collected: {:#?}", collected);
+    debug!("intermidiate: {:#?}", intermidiate);
+
+    let mut ans = HashMap::<usize, BTreeSet<Vec<usize>>>::new();
+    collected.iter_mut().for_each(|(ref_id, accesses)| {
+        while ans.get(ref_id).unwrap_or(&BTreeSet::new()).len()
+            < unsafe { ceilf32(*intermidiate.get(ref_id).unwrap()) as usize }
+        // FIXME: no sure if f32 is enough here
+        {
+            let mut sample_name: Vec<usize> = Vec::new();
+            accesses.iter_mut().for_each(|(_, range)| {
+                let (lb, ub) = (range.start, range.end);
+                let mut rng = rand::thread_rng();
+                let dist = rand::distributions::Uniform::new(0, ub - lb);
+                let rand_num = dist.sample(&mut rng);
+                sample_name.push(rand_num);
+            });
+            ans.entry(*ref_id)
+                .or_insert(BTreeSet::new())
+                .insert(sample_name);
+            // kinda weird here, feels like wasting a cycle of loop
+        }
+    });
+    // ans.clone().into_iter().for_each(|(ref_id, samples)| {
+    //     println!("ref_id: {}", ref_id);
+    //     println!("samples: {}", samples.len());
+    // });
+    print!("ans: {:#?}", ans);
+    ans
 }
--- static_ri/src/sampling.rs
+++ static_ri/src/sampling.rs
@@ -1,13 +1,16 @@
-use crate::ri_utils::access3addr;
-use dace::arybase::set_arybase;
-use dace::ast::{LoopBound, Node, Stmt};
-use fxhash::FxHashMap;
-use hist::Hist;
 use std::collections::hash_map::Entry;
 use std::collections::{BTreeSet, HashMap};
 use std::rc::Rc;
 use std::sync::atomic::{AtomicI64, Ordering};
+
+use fxhash::FxHashMap;
 use tracing::debug;
+
+use dace::arybase::set_arybase;
+use dace::ast::{AryRef, BranchStmt, LoopBound, LoopStmt, Node, Stmt};
+use hist::Hist;
+
+use crate::ri_utils::access3addr;
 
 static COUNTER: AtomicI64 = AtomicI64::new(0);
 
@@ -29,72 +32,100 @@ fn trace_ri(
 ) {
     match &code.stmt {
         Stmt::Ref(ary_ref) => {
-            debug!("trace_ri arr ref: {:#?}", code);
-            let addr = access3addr(ary_ref, ivec) as u64;
-            debug!("addr: {}", addr);
-            let str_name = ary_ref.name.clone();
-            let mut prev_counter: Option<i64> = None;
-            let local_counter = COUNTER.load(Ordering::Relaxed);
-            match LAT_hash.entry(str_name) {
-                Entry::Occupied(mut entry) => {
-                    // *entry.entry(addr).and_.or_insert(0) += 1;
-                    // entry.entry(addr).and_modify(|e| *e += 1).or_insert(counter);
-                    match entry.get_mut().entry(addr) {
-                        Entry::Occupied(mut inner) => {
-                            prev_counter = Some(inner.insert(local_counter));
-                        }
-                        Entry::Vacant(entry) => {
-                            entry.insert(local_counter);
-                        }
-                    }
-                }
-                Entry::Vacant(entry) => {
-                    let mut inner_hash: FxHashMap<u64, i64> = Default::default();
-                    inner_hash.insert(addr, local_counter);
-                    entry.insert(inner_hash);
-                }
-            }
-            let mut ri: Option<_> = None;
-            if let Some(prev_counter) = prev_counter {
-                //update ri
-                ri = Some((local_counter - prev_counter) as usize);
-            } // FIXME: hist seems weird, how to deal with -1(the ri of never accessed again elements)
-            hist.add_dist(ri);
-            COUNTER.fetch_add(1, Ordering::Relaxed);
-            debug!("counter: {}", COUNTER.load(Ordering::Relaxed));
-            debug!("LAT_hash:{:#?}", LAT_hash);
-            debug!("hist: {}", hist);
+            handle_ref_stmt(ary_ref, LAT_hash, ivec, hist);
         }
         Stmt::Loop(aloop) => {
-            // debug!("trace_ri loop ref: {:#?}", code);
-            if let LoopBound::Fixed(lb) = aloop.lb {
-                if let LoopBound::Fixed(ub) = aloop.ub {
-                    (lb..ub).for_each(|i| {
-                        aloop.body.iter().for_each(|stmt| {
-                            let mut myvec = ivec.to_owned();
-                            myvec.push(i);
-                            trace_ri(stmt, LAT_hash, &myvec, hist)
-                        })
-                    })
-                } else {
-                    panic!("dynamic loop upper bound is not supported")
-                }
-            } else {
-                panic!("dynamic loop lower bound is not supported")
-            }
+            handle_loop_stmt(aloop, LAT_hash, ivec, hist);
         }
         Stmt::Block(blk) => {
-            // debug!("trace_ri block ref: {:#?}", code);
-            blk.iter()
-                .for_each(|s| trace_ri(s, LAT_hash, ivec.clone(), hist))
+            blk.iter().for_each(|s| trace_ri(s, LAT_hash, ivec, hist));
         }
         Stmt::Branch(stmt) => {
-            if (stmt.cond)(ivec) {
-                trace_ri(&stmt.then_body, LAT_hash, ivec, hist)
-            } else if let Some(else_body) = &stmt.else_body {
-                trace_ri(else_body, LAT_hash, ivec, hist)
-            }
+            handle_branch_stmt(stmt, LAT_hash, ivec, hist);
         }
+    }
+}
+
+#[allow(non_snake_case)]
+fn handle_ref_stmt(
+    ary_ref: &AryRef,
+    LAT_hash: &mut FxHashMap<String, FxHashMap<u64, i64>>,
+    ivec: &[i32],
+    hist: &mut Hist,
+) {
+    debug!("trace_ri arr ref: {:#?}", ary_ref);
+    let addr = access3addr(ary_ref, ivec) as u64;
+    debug!("addr: {}", addr);
+    let str_name = ary_ref.name.clone();
+    let mut prev_counter: Option<i64> = None;
+    let local_counter = COUNTER.load(Ordering::Relaxed);
+
+    match LAT_hash.entry(str_name) {
+        Entry::Occupied(mut entry) => match entry.get_mut().entry(addr) {
+            Entry::Occupied(mut inner) => {
+                prev_counter = Some(inner.insert(local_counter));
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(local_counter);
+            }
+        },
+        Entry::Vacant(entry) => {
+            let mut inner_hash: FxHashMap<u64, i64> = Default::default();
+            inner_hash.insert(addr, local_counter);
+            entry.insert(inner_hash);
+        }
+    }
+
+    if let Some(prev_counter) = prev_counter {
+        // Update reuse interval
+        let ri = Some((local_counter - prev_counter) as usize);
+        // FIXME: hist seems weird, how to deal with -1(the ri of never accessed again elements)
+        hist.add_dist(ri);
+    } else {
+        hist.add_dist(None);
+    }
+
+    COUNTER.fetch_add(1, Ordering::Relaxed);
+    debug!("counter: {}", COUNTER.load(Ordering::Relaxed));
+    debug!("LAT_hash:{:#?}", LAT_hash);
+    debug!("hist: {}", hist);
+}
+
+#[allow(non_snake_case)]
+fn handle_loop_stmt(
+    aloop: &LoopStmt,
+    LAT_hash: &mut FxHashMap<String, FxHashMap<u64, i64>>,
+    ivec: &[i32],
+    hist: &mut Hist,
+) {
+    if let LoopBound::Fixed(lb) = aloop.lb {
+        if let LoopBound::Fixed(ub) = aloop.ub {
+            for i in lb..ub {
+                let mut myvec = ivec.to_owned();
+                myvec.push(i);
+                for stmt in &aloop.body {
+                    trace_ri(stmt, LAT_hash, &myvec, hist);
+                }
+            }
+        } else {
+            panic!("Dynamic loop upper bound is not supported");
+        }
+    } else {
+        panic!("Dynamic loop lower bound is not supported");
+    }
+}
+
+#[allow(non_snake_case)]
+fn handle_branch_stmt(
+    stmt: &BranchStmt,
+    LAT_hash: &mut FxHashMap<String, FxHashMap<u64, i64>>,
+    ivec: &[i32],
+    hist: &mut Hist,
+) {
+    if (stmt.cond)(ivec) {
+        trace_ri(&stmt.then_body, LAT_hash, ivec, hist);
+    } else if let Some(else_body) = &stmt.else_body {
+        trace_ri(else_body, LAT_hash, ivec, hist);
     }
 }
 
